55a006459b8f75caa20658b209a6ee8cf88a1842
Eduardo-Ofice-Jr <oficejnr@gmail.com>
2024-04-29 22:08:40 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function entrar (){
+    var input_user = document.getElementById('input_user')
+    var input_senha = document.getElementById('input_senha')
+
+/*if (input_user === '' || input_senha === '' || isNaN (input_user) || isNaN (input_senha) ){
+    notie.alert({ text: 'Por favor, preenche os dados'});
+    return;
+
+} Não estou conseguindo usar o notie*/ 
+}
---
+++ createaccount.js
@@ -0,0 +1,10 @@
+function submeter (){
+    var input_user = document.getElementById('input_user')
+    var input_cargo = document.getElementById('input_cargo')
+    var input_senha = document.getElementById('input_senha')
+    var confirmar_senha = document.getElementById('confirmar_senha')
+}
+
+if (input_user === '' || input_cargo === '' || input_senha === '' || confirmar_senha === '' || isNaN(input_user) || isNaN(input_cargo) || isNaN(input_senha) || isNaN (confirmar_senha)){
+    
+}
