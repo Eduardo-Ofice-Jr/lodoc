e3adc8fb0dfc201f41765bafc0d1a18025573f15
Eduardo-Ofice-Jr <oficejnr@gmail.com>
2024-05-02 00:42:26 +0200

    Criação de uma página "create account" e uma página "upload file"

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ function entrar (){
     var input_user = document.getElementById('input_user')
     var input_senha = document.getElementById('input_senha')
 
-/*if (input_user === '' || input_senha === '' || isNaN (input_user) || isNaN (input_senha) ){
-    notie.alert({ text: 'Por favor, preenche os dados'});
+if (input_user === '' || input_senha === '' || isNaN (input_user) || isNaN (input_senha) ){
+    notie.alert({ text: "Por favor, preenche os campos em branco", type: 5});
     return;
+} 
+}
 
-} Não estou conseguindo usar o notie*/ 
+function submeter (){
+    var input_user = document.getElementById('input_user')
+    var input_cargo = document.getElementById('input_cargo')
+    var input_senha = document.getElementById('input_senha')
+    var confirmar_senha = document.getElementById('confirmar_senha')
+}
+
+if (input_user === '' || input_cargo === '' || input_senha === '' || confirmar_senha === '' || isNaN(input_user) || isNaN(input_cargo) || isNaN(input_senha) || isNaN (confirmar_senha)){
+    
 }
