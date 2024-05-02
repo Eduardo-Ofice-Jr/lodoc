2864e3b4d2ba688f322d532636773199e7879394
Eduardo-Ofice-Jr <oficejnr@gmail.com>
2024-05-02 03:53:35 +0200

    Update of the file upload page

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function entrar (){
+/*function entrar (){
     var input_user = document.getElementById('input_user')
     var input_senha = document.getElementById('input_senha')
 
@@ -17,4 +17,23 @@ function submeter (){
 
 if (input_user === '' || input_cargo === '' || input_senha === '' || confirmar_senha === '' || isNaN(input_user) || isNaN(input_cargo) || isNaN(input_senha) || isNaN (confirmar_senha)){
     
-}
+}*/
+
+const inputFile = window.document.getElementById('input_upload')
+const descricaoupload = window.document.getElementById('descricao_upload')
+const descricaouploadTxt = "ESCOLHER ARQIVOS"
+descricaoupload.innerHTML = descricaouploadTxt
+
+inputFile.addEventListener('change', function(e) {
+    const inputTarget = e.target;
+    const file = inputTarget.files[0];
+
+
+    if (file){
+        descricaoupload.innerHTML = "Arquivo Anexado"
+
+    } else{
+        descricaoupload.innerHTML = descricaouploadTxt
+    }
+    console.log(file);
+}) 
